d6f9d9271da462ef14ecfa53fb0eeef67912a934
Caeldan Rodrigues <caeldanrodrigues01@gmail.com>
2023-09-17 20:48:42 +0530

    created netlify serverless function to fetch content from notion db

--- functions/fetchNotion.js
+++ functions/fetchNotion.js
@@ -1,6 +1,34 @@
+const { Client } = require("@notionhq/client");
+
+// fetch from netlify
+const { NOTION_KEY, NOTION_DB } = process.env;
+
+// Initializing a client
+const notion = new Client({
+  auth: NOTION_KEY,
+});
+
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+  try {
+    const response = await notion.databases.query({
+      database_id: NOTION_DB,
+      filter: {
+        property: "Status",
+        select: {
+          equals: "Live",
+        },
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: e.toString(),
+    }
+  }
 };
